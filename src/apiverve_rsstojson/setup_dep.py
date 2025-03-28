09bc3102ba28ad909e8ef5effd45e909ec8e56e7
APIVerve Team <hello@apiverve.com>
2025-03-28 17:47:40 -0500

    Bug Fixes

---
+++ src/apiverve_rsstojson/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_rsstojson',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='RSS to JSON is a simple tool for converting RSS feeds into JSON format. It returns the RSS feed in JSON format.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
